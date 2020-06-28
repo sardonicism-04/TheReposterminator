13193b762711173e193973d9b0035900f8c4f4a6
sardonicism-04 <110789901+sardonicism-04@users.noreply.github.com>
2020-06-28 17:00:00 -0500

    Added cli option to set logging level

--- __main__.py
+++ __main__.py
@@ -1,13 +1,23 @@
 import argparse
 import sys
+import logging
 
-from .bot import BotClient
+from .bot import BotClient, logger
 
 client = BotClient()
+
+logging_level_mapping = {
+    'info': logging.INFO,
+    'debug': logging.DEBUG,
+    'warning': logging.WARNING,
+    'error': logging.ERROR,
+    'critical': logging.CRITICAL
+}
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-r', '--run', action='store_true', help='Runs the bot')
 parser.add_argument('-s', '--show', action='store_true', help='Lists all subreddits the bot is currently active on')
+parser.add_argument('-l', '--level', nargs='?', choices=['info', 'debug', 'warning', 'error', 'critical'])
 
 args = parser.parse_args()
 
@@ -17,3 +27,6 @@ if args.show:
 
 if args.run:
     client.run()
+
+if args.level:
+    logger.setLevel(logging_level_mapping[args.level])
