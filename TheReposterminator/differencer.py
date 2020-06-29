0e9c6082a630571f328bff332baff12ee7276283
sardonicism-04 <110789901+sardonicism-04@users.noreply.github.com>
2020-06-28 22:06:42 -0500

    apply license to all files

--- TheReposterminator/differencer.py
+++ TheReposterminator/differencer.py
@@ -1,3 +1,20 @@
+"""
+TheReposterminator Reddit bot to detect reposts
+Copyright (C) 2020 sardonicism-04
+
+TheReposterminator is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+TheReposterminator is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with TheReposterminator.  If not, see <https://www.gnu.org/licenses/>.
+"""
 from PIL import Image
 
 def diff_hash(image):
