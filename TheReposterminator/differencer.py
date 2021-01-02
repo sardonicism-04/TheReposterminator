c64e4429bd9b61b1934d6ed4189c8314e4961b60
sardonicism-04 <110789901+sardonicism-04@users.noreply.github.com>
2021-01-02 16:54:49 -0600

    Update license year

--- TheReposterminator/differencer.py
+++ TheReposterminator/differencer.py
@@ -1,6 +1,6 @@
 """
 TheReposterminator Reddit bot to detect reposts
-Copyright (C) 2020 sardonicism-04
+Copyright (C) 2021 sardonicism-04
 
 TheReposterminator is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published
