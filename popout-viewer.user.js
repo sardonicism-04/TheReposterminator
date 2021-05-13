3768b6dbbe029ab23c67828f4e0d84f29509002e
sardonicism-04 <110789901+sardonicism-04@users.noreply.github.com>
2021-05-12 22:32:33 -0500

    Bump UserScript version

--- popout-viewer.user.js
+++ popout-viewer.user.js
@@ -4,7 +4,7 @@
 // @author         sardonicism-04
 // @run-at         document-idle
 // @include        https://*.reddit.com/*
-// @version        1.7
+// @version        1.8
 // @icon           https://i.imgur.com/7L31aKL.jpg
 // ==/UserScript==
 
