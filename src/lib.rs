e8494b39129e7b93b7c1aa039a6c0abe7134aeec
sardonicism-04 <110789901+sardonicism-04@users.noreply.github.com>
2021-05-18 19:45:47 -0500

    Clean up Rust code a bit

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,16 @@ use pyo3::wrap_pyfunction;
 // Compare two hashes and return a percent similarity
 #[pyfunction]
 fn compare_hashes(hash1: &str, hash2: &str) -> PyResult<usize> {
+    // hash1 and hash2 are accepted as &str because it results in the
+    // lowest amount of Python conversion operations
     let hash1 = hash1.parse::<usize>()?;
     let hash2 = hash2.parse::<usize>()?;
 
     let hash_xor = format!("{:#b}", (hash1 ^ hash2));
-    let occurences: usize = hash_xor.match_indices("1").collect::<Vec<_>>().len();
+    let occurences: usize = hash_xor
+        .match_indices("1")
+        .collect::<Vec<(usize, &str)>>()
+        .len();
     let result = (((64.0 - occurences as f64) * 100.0) / 64.0) as usize;
 
     Ok(result)
@@ -19,36 +24,41 @@ fn compare_hashes(hash1: &str, hash2: &str) -> PyResult<usize> {
 // But since it's in Rust it does it super speedy fast
 #[pyfunction]
 fn generate_hash(buffer: &[u8]) -> PyResult<usize> {
-    let image_format = match image::guess_format(buffer) {
+    // Avoid panicking in unrecognized formats, return 0 for easy of ignoring
+    let image_original = match image::load_from_memory(buffer) {
         Ok(val) => val,
         Err(_err) => return Ok(0),
     };
-    // Avoid panicking in unrecognized formats
 
-    let image_original = image::load_from_memory_with_format(buffer, image_format).unwrap();
-
-    // Resize to 8x8px, ignore aspect ratio
+    // Resize to 8x8px, ignore aspect ratio, convert to greyscale
     let img = image_original.resize_exact(8, 8, image::imageops::Lanczos3);
     let img = img.to_luma8();
 
-    // Need to create a vector of pixels so it can be indexed
-    let all_pixels: Vec<u8> = img.pixels().cloned().map(|px| px[0]).collect();
-    let mut all_pixels_chunked: Vec<Vec<u8>> =
-        all_pixels.chunks(8).map(|chunk| chunk.to_vec()).collect();
+    // This is a lot of chained method calls, so here's a breakdown:
+    // - Get the image pixels, clone and map a closure on them that gets their value
+    // - Collect that into a Vev<u8>, then chunk that Vec<u8> into chunks of 8
+    // - Collect those chunks into a Vec<Vec<u8>>
+    let mut pixels: Vec<Vec<u8>> = img
+        .pixels()
+        .cloned()
+        .map(|px| px[0])
+        .collect::<Vec<u8>>()
+        .chunks(8)
+        .map(|chunk| chunk.to_vec())
+        .collect();
 
+    // Reverse every other chunk
     for i in (1..8).step_by(2) {
-        let mut to_flip = all_pixels_chunked.remove(i);
+        let mut to_flip = pixels.remove(i);
         to_flip.reverse();
-        all_pixels_chunked.insert(i, to_flip);
+        pixels.insert(i, to_flip);
     }
-
-    let pixels: Vec<u8> = all_pixels_chunked.concat();
 
     let mut prev_px = img.get_pixel(0, 7)[0];
     let mut diff_hash = 0;
 
-    for pixel in pixels {
-        diff_hash <<= 1; // Shift the hash left once each pixel
+    for pixel in pixels.concat() {
+        diff_hash <<= 1;
         diff_hash |= (pixel >= prev_px) as usize;
         prev_px = pixel;
     }
